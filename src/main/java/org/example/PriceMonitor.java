7cdc1678982430867e79ac0fd8a76459a6e875e1
Larian <larian.dam@gmail.com>
2025-02-12 12:12:17 +0000

    Add files via upload

---
+++ src/main/java/org/example/PriceMonitor.java
@@ -0,0 +1,17 @@
+package org.example;
+
+//http
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+//Jsoup
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+
+public class PriceMonitor {
+    public static void main(String[] args) {
+
+    }
+}
